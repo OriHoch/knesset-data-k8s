0d8ed14df2252fd527f4d396598df2ec17b530c3
Ori Hoch <ori@uumpa.com>
2018-07-03 12:14:23 +0300

    modify service account to hasadna cluster

--- .travis_deploy.sh
+++ .travis_deploy.sh
@@ -6,10 +6,15 @@ openssl aes-256-cbc -K $encrypted_2cdba463b699_key -iv $encrypted_2cdba463b699_i
 K8S_ENVIRONMENT_NAME="production"
 OPS_REPO_SLUG="OriHoch/knesset-data-k8s"
 OPS_REPO_BRANCH="${TRAVIS_BRANCH}"
-./run_docker_ops.sh "${K8S_ENVIRONMENT_NAME}" "
+./run_docker_ops.sh "${K8S_ENVIRONMENT_NAME}" '
     RES=0;
     curl -L https://raw.githubusercontent.com/hasadna/hasadna-k8s/master/apps_travis_script.sh | bash /dev/stdin install_helm;
-    if ./helm_upgrade_all.sh --install --dry-run --debug; then
+    if echo "'"${TRAVIS_COMMIT_MESSAGE}"'" | grep "automatic update of knesset-data-pipelines"; then
+        kubectl set image --dry-run -o yaml deployment/pipelines "pipelines=$(eval echo $(./read_env_yaml.sh pipelines image))" &&\
+        kubectl set image deployment/pipelines "pipelines=$(eval echo $(./read_env_yaml.sh pipelines image))" &&\
+        bash charts-external/pipelines/healthcheck.sh
+        [ "$?" != "0" ] && echo failed to update pipelines image && exit 1
+    elif ./helm_upgrade_all.sh --install --dry-run --debug; then
         echo Dry run was successfull, performing upgrades
         ! ./helm_upgrade_all.sh --install && echo Failed upgrade && RES=1
         ! ./helm_healthcheck.sh && echo Failed healthcheck && RES=1
@@ -20,8 +25,8 @@ OPS_REPO_BRANCH="${TRAVIS_BRANCH}"
     sleep 2;
     kubectl get pods --all-namespaces;
     kubectl get service --all-namespaces;
-    exit "'$'"RES
-" "orihoch/sk8s-ops" "${OPS_REPO_SLUG}" "${OPS_REPO_BRANCH}" "secret-k8s-ops.json"
+    exit "$RES"
+' "orihoch/sk8s-ops" "${OPS_REPO_SLUG}" "${OPS_REPO_BRANCH}" "secret-k8s-ops.json"
 if [ "$?" == "0" ]; then
     echo travis deployment success
     exit 0
