65a5dae1ceb3c86a71517f9310e3c88fa0952c72
Ori Hoch <ori@uumpa.com>
2020-03-11 13:09:14 +0200

    change NFS server --no-deploy

--- helm_lint_all.sh
+++ helm_lint_all.sh
@@ -4,7 +4,12 @@ RES=0
 
 echo "Linting all charts"
 for CHART_NAME in `ls charts-external`; do
-    helm lint charts-external/$CHART_NAME 2>/dev/null | grep 'ERROR' | grep -v 'Chart.yaml: version is required'
+    if [ "$(eval echo `./read_yaml.py "charts-external/${CHART_NAME}/Chart.yaml" apiVersion 2>/dev/null`)" == "v2" ]; then
+          HELM_BIN=helm3
+        else
+          HELM_BIN=helm
+    fi
+    $HELM_BIN lint charts-external/$CHART_NAME 2>/dev/null | grep 'ERROR' | grep -v 'Chart.yaml: version is required'
     if [ "$?" == "0" ]; then
         echo "${CHART_NAME}: failed lint"
         RES=1
